c219345024d870946bc4a3bb1e8775b79f00ba77
RostaTasha <natascha@rosta.ru>
2016-08-19 22:00:07 +0300

    small fix

--- src/xpiks-qt/SpellCheck/spellcheckworker.cpp
+++ src/xpiks-qt/SpellCheck/spellcheckworker.cpp
@@ -185,7 +185,7 @@ namespace SpellCheck {
             }
 
             qInfo() <<  "Cleaning user dictionary";
-            for (const QString &word = stream.readLine(); !word.isEmpty(); word = stream.readLine()) {
+            for (QString word = stream.readLine(); !word.isEmpty(); word = stream.readLine()) {
                 removeWord(word);
             }
 
