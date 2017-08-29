89b65b048b6ec0f45a55715419a62a4c15b9786d
Taras Kushnir <kushnirTV@gmail.com>
2017-08-29 09:53:44 +0000

    Reverted assert

--- src/xpiks-qt/MetadataIO/cachedartwork.cpp
+++ src/xpiks-qt/MetadataIO/cachedartwork.cpp
@@ -106,7 +106,7 @@ namespace MetadataIO {
 
     QDataStream &operator<<(QDataStream &out, const CachedArtwork &v) {
         // TODO: update before release to Qt 5.9
-        Q_ASSERT(XPIKS_VERSION_CHECK(1, 5, 0));
+        Q_ASSERT(!XPIKS_VERSION_CHECK(1, 5, 0));
 #ifndef TRAVIS_CI
         out.setVersion(QDataStream::Qt_5_6);
 #endif
@@ -145,7 +145,7 @@ namespace MetadataIO {
 
     QDataStream &operator>>(QDataStream &in, CachedArtwork &v) {
         // TODO: update before release to Qt 5.9
-        Q_ASSERT(XPIKS_VERSION_CHECK(1, 5, 0));
+        Q_ASSERT(!XPIKS_VERSION_CHECK(1, 5, 0));
 #ifndef TRAVIS_CI
         in.setVersion(QDataStream::Qt_5_6);
 #endif
